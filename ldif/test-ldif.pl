27ab04eac87818ce886f963df893a532eeb8b72e
Alexei Znamensky <russoz@cpan.org>
2010-11-05 10:53:30 -0200

    added simple ldif test

---
+++ ldif/test-ldif.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Net::LDAP::LDIF;
+use Data::Dumper;
+
+my $file = shift;
+my $ldif = Net::LDAP::LDIF->new($file);
+
+while ( not $ldif->eof() ) {
+    my $entry = $ldif->read_entry();
+    if ( $ldif->error() ) {
+        print STDERR "Error msg: ",    $ldif->error(),       "\n";
+        print STDERR "Error lines:\n", $ldif->error_lines(), "\n";
+        next;
+    }
+    say Dumper($entry);
+}
+$ldif->done();
+
