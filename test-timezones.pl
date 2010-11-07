d1a473ca043b8659c192b693a8db9c6a6a303d0d
Alexei Znamensky <russoz@cpan.org>
2010-11-07 17:12:18 -0200

    testing different timezones handling in perl

--- test-timezones.pl
+++ test-timezones.pl
@@ -1,7 +1,23 @@
 #!/usr/bin/env perl
 
-use DateTime::TimeZone;
+use 5.010;
 
-my $tz = DateTime::TimeZone->new( name => 'local' );
+use DateTime;
 
-print 'tz = '. $tz->name . "\n";
+sub showtz {
+    local $ENV{TZ} = $_[0] if $_[0];
+    my $now = DateTime->now->set_time_zone('local');
+
+    say '-' x 40;
+    say 'env.tz = ' . $ENV{TZ};
+    say 'tz     = ' . $now->time_zone()->name;
+    say 'time   = ' . $now->hms;
+}
+
+showtz;
+
+showtz( 'America/Los_Angeles' );
+showtz( 'Europe/Paris' );
+showtz( 'US/Pacific' );
+
+showtz;
