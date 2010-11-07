ac58d1d74f2768873e7499bf52cd1631fab23971
Alexei Znamensky <russoz@cpan.org>
2010-11-07 00:07:04 -0200

    testing brazilian authors

---
+++ test-br-authors.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use 5.010;
+use Data::Dumper;
+use Acme::CPANAuthors;
+use Acme::CPANAuthors::Brazilian;
+
+my $authors = Acme::CPANAuthors->new('Brazilian');
+
+my $number   = $authors->count;
+my @ids      = $authors->id;
+my @distros  = $authors->distributions('RUSSOZ');
+my $url      = $authors->avatar_url('RUSSOZ');
+my $kwalitee = $authors->kwalitee('RUSSOZ');
+
+
+say $numbers;
+say Dumper(@ids);
+say Dumper(@distros);
+say $url;
+say $kwalitee;
+
