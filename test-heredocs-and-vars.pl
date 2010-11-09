f283e472dffb764d3ecfd7ca1fa56999726a17f9
Alexei Znamensky <russoz@cpan.org>
2010-11-09 20:43:54 -0200

    added test-heredocs-and-vars.pl and renamed ldif/ to test-ldif/

---
+++ test-heredocs-and-vars.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use 5.010;
+use Data::Dumper;
+
+sub mktext {
+    my $spec = shift;
+    print Data::Dumper->Dump([$spec], ['spec']);
+
+    return <<"EOF";
+The lazy $spec->{animal} jumped over the qq{spec->{thing}} and crashed
+over the neat "$spec->{landingpad}".
+EOF
+}
+
+say mktext({ animal => 'fox', thing => 'fence', landingpad => 'sofa' });
+
