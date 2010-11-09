4c7f1b1fe256d62d9b99a697bb0f29e6d47b772a
Alexei Znamensky <russoz@cpan.org>
2010-11-09 18:57:39 -0200

    test for File::Spec
    show how files and directories are put together in different OS'es

---
+++ test-file-spec.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+sub mostra {
+    my $os = shift;
+    {
+        eval qq{use File::Spec::$os; say $os . "\t"
+          . File::Spec::$os->catfile( File::Spec::$os->rootdir(), 'tmp', 'grande',
+            'abobora' );
+          };
+        # File::Spec::VMS is known to fail - if you really wanna see the error
+        #   message, uncomment the line below
+        #warn $@ if $@;
+    }
+}
+
+foreach my $os qw{Unix Mac OS2 Win32 VMS Epoc Cygwin} {
+    mostra($os);
+}
+
