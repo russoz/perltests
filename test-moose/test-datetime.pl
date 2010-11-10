de78424dab84d53f72b5c974588b444c091c8996
Alexei Znamensky <russoz@cpan.org>
2010-11-09 22:39:20 -0200

    first test on how moose handles things

---
+++ test-moose/test-datetime.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use 5.010;
+use Data::Dumper;
+
+package MyPkg;
+
+use Moose;
+
+has 'blubs' => (
+    is       => 'rw',
+    isa      => 'Int',
+    required => 1,
+);
+
+has 'timestamp' => (
+    is      => 'ro',
+    isa     => 'DateTime',
+    default => sub {
+        use DateTime;
+        return DateTime->now;
+    },
+    lazy => 1,
+);
+
+package main;
+
+use DateTime;
+
+say DateTime->now->set_time_zone('local');
+
+my $a = MyPkg->new( { blubs => 3 } );
+
+print Data::Dumper->Dump( [$a], ['a'] );
+say '$a->blubs = ' . $a->blubs();
+
+sleep(10);
+
+say $a->timestamp->set_time_zone('local');
+
