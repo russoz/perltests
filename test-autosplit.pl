68bd14037710d5a7783cb2c225cc6c00df169f0a
Alexei Znamensky <russoz@cpan.org>
2010-11-13 14:31:46 -0200

    testing autosplit

---
+++ test-autosplit.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#
+# usage:
+#   du -ks * | ./test-autosplit.pl
+#
+
+use 5.010;
+use Data::Dumper;
+
+$u = {};
+
+while (<>) {
+    if( /^(\d+)\s+(\S+)\s*$/ ) {
+        $u->{$2} = $1;
+    }
+}
+
+print Dumper($u);
+
