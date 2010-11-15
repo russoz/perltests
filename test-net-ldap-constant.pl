ef4258f2418645496f633a301bc7f4be03fda3b1
Alexei Znamensky <russoz@cpan.org>
2010-11-15 13:45:13 -0200

    added test for Net::LDAP::Constant

---
+++ test-net-ldap-constant.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use Carp;
+use Data::Dumper;
+use Scalar::Util qw{looks_like_number};
+
+sub _make_result {
+    my $code = shift;
+    my $dn   = shift || '';
+    my $msg  = shift || '';
+
+    my $retcode =
+      looks_like_number($code)
+      ? $code
+      : eval qq{
+            use Net::LDAP::Constant qw|$code|;
+            $code;
+        };
+    croak $@ if $@;
+    return {
+        matchedDN    => $dn,
+        errorMessage => $msg,
+        resultCode   => $retcode,
+    };
+}
+
+$a = _make_result( qw/LDAP_INAPPROPRIATE_AUTH/ );
+
+print Dumper($a);
