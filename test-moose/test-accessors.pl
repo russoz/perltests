ea02e4947d41c6cd2804edb158cb9778accb3562
Alexei Znamensky <russoz@cpan.org>
2010-12-02 15:05:20 -0200

    dumb accessor test for moose

---
+++ test-moose/test-accessors.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use 5.010;
+use Data::Dumper;
+
+package A;
+
+    use Moose;
+
+    has intguy => ( is => 'rw', isa => 'Int' );
+    has strguy => ( is => 'rw', isa => 'Str' );
+
+package main;
+
+my $a = A->new;
+
+$a->intguy( '42' );
+$a->strguy( 57 );
+
+print Dumper($a);
+
+print $a->intguy(1984);
+print "\n";
+
+print Dumper($a);
+
