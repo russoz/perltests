9ce98e85d3ec8d39642df97e96dd34db60631f0d
Alexei Znamensky <russoz@cpan.org>
2010-12-09 14:11:03 -0200

    tidy

--- test-file-basename.pl
+++ test-file-basename.pl
@@ -5,11 +5,11 @@ use File::Basename;
 
 my $filename = '/some/where/over/the/rainbow/theres.a.place.txt';
 
-say 'filename = '.$filename;
+say 'filename = ' . $filename;
 $, = ', ';
-say 'no suffix -> ', fileparse($filename);
-say 'suf (txt) -> ', fileparse($filename, qw/txt/);
-say 'suf (zip) -> ', fileparse($filename, qw/zip/);
-say 'suf (place txt) -> ', fileparse($filename, qw/place txt/);
-say 'suf (place.txt) -> ', fileparse($filename, qw/place.txt/);
+say 'no suffix -> ',       fileparse($filename);
+say 'suf (txt) -> ',       fileparse( $filename, qw/txt/ );
+say 'suf (zip) -> ',       fileparse( $filename, qw/zip/ );
+say 'suf (place txt) -> ', fileparse( $filename, qw/place txt/ );
+say 'suf (place.txt) -> ', fileparse( $filename, qw/place.txt/ );
 
--- test-grep.pl
+++ test-grep.pl
@@ -6,7 +6,6 @@ my @numbers = qw/0 1 2 3 4 5 6 7 8 9/;
 
 my @result = grep { $_ < 5 } @numbers;
 
-
 $, = ' ';
 say 'numbers =', @numbers;
 say 'result  =', @result;
