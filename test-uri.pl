0e5738d7da936bf71f3c88dff2915b34e865bff3
Alexei Znamensky <russoz@cpan.org>
2010-12-12 20:27:31 -0200

    testing URI->new_abs

---
+++ test-uri.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use 5.010;
+use URI;
+
+$base = 'http://www.portaltransparencia.gov.br/convenios/ConveniosListaGeral.asp?Ordem=-1';
+$link = 'DetalhaConvenio.asp?TipoConsulta=5&CodConvenio=900154';
+
+$result = 'http://www.portaltransparencia.gov.br/convenios/DetalhaConvenio.asp?TipoConsulta=5&CodConvenio=900154';
+
+my $uri = URI->new_abs($link, $base);
+
+say 'uri = '. $uri->as_string;
