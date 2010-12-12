383888efbb2da1c8ce835c0aece33a8f765c09fc
Alexei Znamensky <russoz@cpan.org>
2010-12-12 19:38:40 -0200

    simple test with predicates and layz attributes

---
+++ test-moose/test-predicate.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+package P;
+    use Moose;
+
+    has num => (
+        is => 'ro',
+        isa => 'Int',
+        required => 1,
+        lazy => 1,
+        default => 42,
+        predicate => 'has_num',
+    );
+
+package main;
+
+use Data::Dumper;
+
+my $p = P->new;
+
+print 'before     = ' . Dumper($p);
+say   'p->has_num = ' . $p->has_num;
+say   'p->num     = ' . $p->num;
+print 'after      = ' . Dumper($p);
