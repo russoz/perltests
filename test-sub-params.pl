d873ed5df44b850e68078ccafe56587a55400f8a
Alexei Znamensky <russoz@cpan.org>
2010-12-20 00:02:13 -0200

    simple test for a sub parameters
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-sub-params.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+sub somesub {
+    say '=' x 40;
+    for( $i = 0; $i < $#_; $i++ ) {
+        say '['.$i.']: '.$_[$i];
+    }
+}
+
+somesub(qw/a b c d e f g h i j k/);
+somesub( 9, 8, 7, 6, 5 );
+somesub('aaa', 'bbb', undef, 'ddd' );
+
