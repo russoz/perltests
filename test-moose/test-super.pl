5e9a0fce6e4acaf4532008365c41e1d71812540c
Alexei Znamensky <russoz@cpan.org>
2010-12-20 08:53:02 -0200

    inheritance, super, with Moose. way too silly, but hey, why not

---
+++ test-moose/test-super.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+package A;
+
+    use Moose;
+    has attr => ( is => 'rw', isa => 'Str',);
+    sub thereis { say 'A::thereis'; return shift; }
+
+package B;
+
+    use Moose; extends 'A';
+    has sky => ( is => 'rw', isa => 'Str',);
+    sub another {
+        my $me = shift;
+        A::thereis($me);
+        say 'B::another '.@_;
+        return $me;
+    }
+
+package main;
+
+my $a = A->new;
+$a->thereis();
+say $a->attr('blablabla');
+
+my $b = B->new;
+$b->thereis->another->sky('walker');
+say $b->attr('blobloblo');
+say $b->sky;
