c4692cdd5a9112284bde3c02ccf786a8b0abcc5c
Alexei Znamensky <russoz@cpan.org>
2011-02-10 17:19:18 -0200

    added more tricks
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-sub-param-trick.pl
+++ test-sub-param-trick.pl
@@ -2,7 +2,7 @@
 
 use 5.010;
 
-my $trick = sub { say 'ss = '.$ss; say 'tt = '.$tt; };
+our $trick = sub { say 'ss = '.$ss; say 'tt = '.$tt; };
 
 sub do_trick {
     my ($ss, $tt) = @_;
@@ -10,6 +10,33 @@ sub do_trick {
     $trick->();
 }
 
+sub better_trick {
+#    my ($ss, $tt) = @_;
+#
+#    no strict 'refs';
+#
+    use vars qw($ss $tt);
+#    my $caller = caller;
+#
+#    local(*{$caller.'::ss'}) = \$ss;
+#    local(*{$caller.'::tt'}) = \$tt;
+#
+#    $trick->();
+}
+
 $ss = 'yo dawg';
 $tt = 'wassup?';
-do_trick
+do_trick;
+
+
+use strict;
+use warnings;
+
+
+$trick = sub { say 'strict ss = '.$ss; say 'strict tt = '.$tt; };
+$ss = 'strict';
+$tt = 'and tight here';
+do_trick;
+
+# works because of the 'use vars' clause :-o)
+
