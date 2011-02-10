9a339eceb4d9644039a5209f517a4ddbfb322e4e
Alexei Znamensky <russoz@cpan.org>
2011-02-10 18:06:10 -0200

    improved the version tester
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-version.pl
+++ test-version.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
 use 5.010;
-
 use version;
 
-$v1 = qv('1.2_01');
-$v2 = qv('1.2');
-say 'v1 < v2' if $v1 < @v2;
+die q{usage: test-version v1 v2 [ v3 [ v4 [ ... ] ] ]} unless scalar @ARGV > 1;
+
+@ordered = sort { version->parse($a) <=> version->parse($b) } @ARGV;
+map { say version->parse($_)->stringify } @ordered;
