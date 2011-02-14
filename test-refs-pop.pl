63de68ab6a0eb96bfb9d60b0c4440c85c0597acc
Alexei Znamensky <russoz@cpan.org>
2011-02-14 14:13:17 -0200

    testing popping up elements from array which is copied from ref
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-refs-pop.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use Data::Dumper;
+
+$r = [ qw/aa bb cc dd ee/ ];
+
+print 'ref (before) = '.Dumper($r);
+
+@a = @{ $r };
+print 'array (before) = ' . Dumper(@a);
+
+pop @a;
+print 'array (after) = ' . Dumper(@a);
+
+print 'ref (before) = '.Dumper($r);
