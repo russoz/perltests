d7737e0a34e31252cca1887e9a2e1afdb6565703
Alexei Znamensky <russoz@cpan.org>
2011-04-07 15:05:03 -0300

    removed #-args restraint
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-version.pl
+++ test-version.pl
@@ -3,7 +3,5 @@
 use 5.010;
 use version;
 
-die q{usage: test-version v1 v2 [ v3 [ v4 [ ... ] ] ]} unless scalar @ARGV > 1;
-
 @ordered = sort { version->parse($a) <=> version->parse($b) } @ARGV;
 map { say version->parse($_)->stringify } @ordered;
