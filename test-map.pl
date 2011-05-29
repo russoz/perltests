d808f5234c1ebefdcc7058aac39a21530d1688db
Alexei Znamensky <russoz@cpan.org>
2011-05-29 19:42:07 -0300

    modified test-grep and added test-map to show the diff between them
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-map.pl
+++ test-map.pl
@@ -4,7 +4,7 @@ use 5.010;
 
 my @numbers = qw/0 1 2 3 4 5 6 7 8 9/;
 
-my @result = grep { $_ > 4 ? 2 * $_ : 0 * $_ } @numbers;
+my @result = map { $_ > 4 ? 2 * $_ : 0 * $_ } @numbers;
 
 $, = ' ';
 say 'numbers =', @numbers;
