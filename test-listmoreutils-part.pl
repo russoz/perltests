438ca73415fc120fb2707a30211ae1f2e3002a01
Alexei Znamensky <russoz@cpan.org>
2011-05-29 12:42:40 -0300

    testing List::MoreUtils::part
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-listmoreutils-part.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+use Data::Dumper;
+use List::MoreUtils qw(part);
+
+sub dmp {
+	print Dumper(\@_);
+}
+
+my @list = qw/aa bb cc dd ee ff gg hh ii jj/;
+
+dmp(@list);
+
+$i = 0;
+dmp( part { $i++ %2 } 1 .. 8 );
+
+$i = 0;
+dmp( part { $i++ % 3 } 0 .. $#list );
+
+$i = 0;
+dmp( part { $list[$i++ % 3] } 0 .. $#list );
+
+$i = 0;
+dmp( part { $i % 3 } @list );
