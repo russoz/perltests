c15f5c21ff74f81675f37e580d4fc758e23a54f6
Alexei Znamensky <russoz@cpan.org>
2011-06-05 18:00:07 -0300

    teste com local e $_
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-arg.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+package A;
+
+sub makesub {
+    my $sub = shift;
+
+    return sub {
+        my $arg = shift;
+        local $_ = $arg;
+        $sub->();
+      };
+}
+
+package main;
+
+say A::makesub( sub { $_ } )->('abcdefghij');
+say A::makesub( sub { uc } )->('abcdefghij');
+say A::makesub( sub { scalar reverse } )->('abcdefghij');
+
