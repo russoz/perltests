e1b3e0b01a0a770b57cef7aa0629cf5bfd4c75cd
Alexei Znamensky <russoz@cpan.org>
2011-06-09 17:54:03 -0300

    more tests with arrayrefs
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-arrayref.pl
+++ test-arrayref.pl
@@ -1,13 +1,26 @@
 #!/usr/bin/perl
 
 use 5.010;
-use Data::Dumper;
+use Data::Printer colored => 0;
 
 $ref = [ 1, 3, 5, 7, 'a', 'yabadabadoo', ];
 
-say Data::Dumper->Dump( [$ref], ['ref'], );
+say antes => p $ref;
 
 push @{$ref}, 'blabla';
 
-say depois => undef;
-say Data::Dumper->Dump( [$ref], ['ref'], );
+say depois => p $ref;
+
+#############################################################################
+say '--->8---' x 9;
+
+$ref = [qw/a b c d e/];
+
+p $ref;
+p $ref->[0];
+p $ref->[1,2];
+p $ref->[1 ... 2];
+say 'deref+index first ', p ${$ref}[0];
+say 'deref+index last ', p ${$ref}[-1];
+say 'deref+indices-comma ', (${$ref}[0,1]);
+say 'deref+indices-ellip ', (${$ref}[0 ... 3]);
