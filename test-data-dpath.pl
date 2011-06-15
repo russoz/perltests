bea9eaa2bd85ef1e7bb865bd57abc0458f53d128
Alexei Znamensky <russoz@cpan.org>
2011-06-15 18:49:55 -0300

    testing Data::DPath
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-data-dpath.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use 5.010;
+use Data::Printer colored => 0;
+
+use Data::DPath qw/dpath dpathr/;
+
+my $data = {
+	aList => [ qw/aa bb cc dd ee ff gg hh ii jj/ ],
+	aHash => {
+		apple => 'pie',
+		banana => 'split',
+		potato => [ qw(baked chips fries fish&chips mashed) ],
+	},
+};
+
+sub pick {
+	my ($caption,$dpath) = @_;
+	say '::: '. $caption;
+	my @res = dpath( $dpath )->match($data);
+	p @res;
+}
+
+# so
+say 'we all';
+p $data;
+
+############################################################################
+
+pick( 'list', '/aList' );
+pick( 'list element', '/aList/*[2]' );
+
+pick( 'all 4th elements of all lists', '//*[3]' );
+
+say '::: tweaking list element';
+my @elem = dpathr( '/aList/*[2]' )->match($data);
+say @elem;
+${$elem[0]} = 'WAKAWAKAWAKA';
+p @elem;
+pick( 'list after tweaking', '/aList' );
+
+############################################################################
+
+pick( 'hash', '/aHash' );
+say 'hash address: '.$data->{aHash};
+pick( q{elements with letter 'i'}, '//*[ value =~ /i/ ]' );
+pick( q{elements with letter 'f'}, '//*[ value =~ /f/ ]' );
