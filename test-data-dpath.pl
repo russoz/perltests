563d932fd52bc2e4e4b4bc0374f03f34776c9d69
Alexei Znamensky <russoz@cpan.org>
2011-06-15 19:14:10 -0300

    perltidy
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- test-data-dpath.pl
+++ test-data-dpath.pl
@@ -6,19 +6,19 @@ use Data::Printer colored => 0;
 use Data::DPath qw/dpath dpathr/;
 
 my $data = {
-	aList => [ qw/aa bb cc dd ee ff gg hh ii jj/ ],
-	aHash => {
-		apple => 'pie',
-		banana => 'split',
-		potato => [ qw(baked chips fries fish&chips mashed) ],
-	},
+    aList => [qw/aa bb cc dd ee ff gg hh ii jj/],
+    aHash => {
+        apple  => 'pie',
+        banana => 'split',
+        potato => [qw(baked chips fries fish&chips mashed)],
+    },
 };
 
 sub pick {
-	my ($caption,$dpath) = @_;
-	say '::: '. $caption;
-	my @res = dpath( $dpath )->match($data);
-	p @res;
+    my ( $caption, $dpath ) = @_;
+    say '::: ' . $caption;
+    my @res = dpath($dpath)->match($data);
+    p @res;
 }
 
 # so
@@ -27,21 +27,21 @@ p $data;
 
 ############################################################################
 
-pick( 'list', '/aList' );
+pick( 'list',         '/aList' );
 pick( 'list element', '/aList/*[2]' );
 
 pick( 'all 4th elements of all lists', '//*[3]' );
 
 say '::: tweaking list element';
-my @elem = dpathr( '/aList/*[2]' )->match($data);
+my @elem = dpathr('/aList/*[2]')->match($data);
 say @elem;
-${$elem[0]} = 'WAKAWAKAWAKA';
+${ $elem[0] } = 'WAKAWAKAWAKA';
 p @elem;
 pick( 'list after tweaking', '/aList' );
 
 ############################################################################
 
 pick( 'hash', '/aHash' );
-say 'hash address: '.$data->{aHash};
+say 'hash address: ' . $data->{aHash};
 pick( q{elements with letter 'i'}, '//*[ value =~ /i/ ]' );
 pick( q{elements with letter 'f'}, '//*[ value =~ /f/ ]' );
