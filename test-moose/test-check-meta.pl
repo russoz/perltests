a19950532bbfb8e5dc5c9c3eccef63e5f7f0ddf1
Alexei Znamensky <russoz@cpan.org>
2011-07-16 20:38:27 -0300

    added test to display meta class information
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ test-moose/test-check-meta.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+package ABC;
+
+use Moose;
+
+has a => (
+ is => 'ro',
+ isa => 'Str',
+);
+
+package main;
+
+say 'has meta' if ABC->meta;
+
+use Data::Dumper;
+print Dumper( ABC->meta );
